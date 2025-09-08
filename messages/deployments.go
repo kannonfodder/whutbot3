7e687dc11795b5394f3966ef1b5849be994d1275
kannonfodder <kannonfodder@kannonfoundry.dev>
2025-09-08 16:32:27 +0100

    attempt to add restarting

---
+++ messages/deployments.go
@@ -0,0 +1,99 @@
+package messages
+
+import (
+	"context"
+	"fmt"
+	"strings"
+
+	"github.com/bwmarrin/discordgo"
+	k8sErrors "k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/types"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
+)
+
+func handleDeploymentsCommand(s *discordgo.Session, m *discordgo.MessageCreate, args string) {
+	command, arguments := ParseCommand(args)
+	switch command {
+	case "list":
+		handleDeploymentsListCommand(s, m, arguments)
+	case "restart":
+		handleDeploymentsRestartCommand(s, m, arguments)
+	default:
+		s.ChannelMessageSend(m.ChannelID, "Unknown deployments command")
+	}
+}
+
+func handleDeploymentsListCommand(s *discordgo.Session, m *discordgo.MessageCreate, args string) {
+
+	msg := "Listing all deployments..."
+	if args != "" {
+		msg += " in args"
+	}
+
+	s.ChannelMessageSend(m.ChannelID, msg)
+
+	clientset, err := getClientSet(s, m)
+	if err != nil {
+		return
+	}
+	deps, err := clientset.AppsV1().Deployments(args).List(context.TODO(), metav1.ListOptions{})
+	if k8sErrors.IsNotFound(err) {
+		s.ChannelMessageSend(m.ChannelID, "Failed to get deployment - not found")
+		fmt.Printf("%s", err.Error())
+	} else if statusError, isStatus := err.(*k8sErrors.StatusError); isStatus {
+		fmt.Printf("%s", statusError.ErrStatus.Message)
+	} else if err != nil {
+		fmt.Printf("Error getting pod %v\n", err)
+	} else {
+		var depNames []string
+		for _, dep := range deps.Items {
+			depNames = append(depNames, fmt.Sprintf("%s - %s - %s", dep.Namespace, dep.Name, dep.Status.String()))
+		}
+		s.ChannelMessageSend(m.ChannelID, fmt.Sprintf("Deployments found:\n%s", strings.Join(depNames, "\n")))
+		return
+	}
+	s.ChannelMessageSend(m.ChannelID, "Failed to get deployment")
+
+}
+
+func handleDeploymentsRestartCommand(s *discordgo.Session, m *discordgo.MessageCreate, args string) {
+	clientset, err := getClientSet(s, m)
+	if err != nil {
+		return
+	}
+	if args == "" {
+		s.ChannelMessageSend(m.ChannelID, "Please specify a namespace and deployment")
+	}
+	parts := strings.SplitN(args, " ", 3)
+	if len(parts) < 2 {
+		s.ChannelMessageSend(m.ChannelID, "Please specify a namespace and deployment")
+		return
+	}
+	namespace := parts[0]
+	deployment := parts[1]
+	s.ChannelMessageSend(m.ChannelID, fmt.Sprintf("Restarting deployment %s in namespace %s", deployment, namespace))
+	_, err = clientset.AppsV1().Deployments(namespace).Patch(context.TODO(), deployment,
+		types.JSONPatchType, []byte(`[{"op": "add", "path": "/spec/template/metadata/annotations/restartedAt", "value":"`+fmt.Sprintf("%d", metav1.Now().Unix())+`"}]`),
+		metav1.PatchOptions{})
+	if err != nil {
+		s.ChannelMessageSend(m.ChannelID, "Failed to restart deployment")
+		return
+	}
+	s.ChannelMessageSend(m.ChannelID, "Deployment restarted successfully")
+}
+
+func getClientSet(s *discordgo.Session, m *discordgo.MessageCreate) (*kubernetes.Clientset, error) {
+	config, err := rest.InClusterConfig()
+	if err != nil {
+		s.ChannelMessageSend(m.ChannelID, "Failed to create in-cluster config")
+		return nil, err
+	}
+	clientset, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		s.ChannelMessageSend(m.ChannelID, "Failed to create k8s client")
+		return nil, err
+	}
+	return clientset, nil
+}
--- messages/k8s.go
+++ messages/k8s.go
@@ -1,65 +1,42 @@
 package messages
 
 import (
-	"context"
 	"fmt"
 	"strings"
 
 	"github.com/bwmarrin/discordgo"
-	k8sErrors "k8s.io/apimachinery/pkg/api/errors"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/client-go/kubernetes"
-	"k8s.io/client-go/rest"
 )
+
+func ParseCommand(input string) (command string, arguments string) {
+	parts := strings.SplitN(input, " ", 2)
+	if len(parts) > 1 {
+		return parts[0], parts[1]
+	}
+	return parts[0], ""
+}
 
 func HandleK8sMessage(s *discordgo.Session, m *discordgo.MessageCreate) {
 	if m.Author == nil || m.Author.Bot {
 		return
 	}
-	firstIdx := strings.Index(m.Content, " ")
-	command := m.Content[:firstIdx]
-	rest := m.Content[firstIdx+1:]
 
-	fmt.Printf("Command: '%s', Rest: '%s'", command, rest)
+	command, arguments := ParseCommand(m.Content)
+
+	fmt.Printf("Command: '%s', Rest: '%s'", command, arguments)
 	switch command {
 	case "ping":
 		s.ChannelMessageSend(m.ChannelID, "Pong")
 	case "get":
-		handleGetCommand(s, m, rest)
+		handleGetCommand(s, m, arguments)
 	default:
 		s.ChannelMessageSend(m.ChannelID, "Unknown command")
 	}
 }
 
 func handleGetCommand(s *discordgo.Session, m *discordgo.MessageCreate, restOfCommand string) {
-	if restOfCommand == "deployments" {
-		// creates the in-cluster config
-		config, err := rest.InClusterConfig()
-		if err != nil {
-			s.ChannelMessageSend(m.ChannelID, "Failed to create in-cluster config")
-			return
-		}
-		clientset, err := kubernetes.NewForConfig(config)
-		if err != nil {
-			s.ChannelMessageSend(m.ChannelID, "Failed to create k8s client")
-			return
-		}
-		deps, err := clientset.AppsV1().Deployments("").List(context.TODO(), metav1.ListOptions{})
-		if k8sErrors.IsNotFound(err) {
-			s.ChannelMessageSend(m.ChannelID, "Failed to get deployment - not found")
-			fmt.Printf("%s", err.Error())
-		} else if statusError, isStatus := err.(*k8sErrors.StatusError); isStatus {
-			fmt.Printf("%s", statusError.ErrStatus.Message)
-		} else if err != nil {
-			fmt.Printf("Error getting pod %v\n", err)
-		} else {
-			for _, dep := range deps.Items {
-				s.ChannelMessageSend(m.ChannelID, fmt.Sprintf("Deployment found: %s", dep.Name))
-			}
-			return
-		}
-		s.ChannelMessageSend(m.ChannelID, "Failed to get deployment")
-		return
+	command, arguments := ParseCommand(restOfCommand)
+	if command == "deployments" {
+		handleDeploymentsCommand(s, m, arguments)
 	} else {
 		s.ChannelMessageSend(m.ChannelID, "Unknown resource")
 	}
